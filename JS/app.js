e88ae73951c00d338c17db21fc5ff9a8fa04527d
ThonkPT <57663757+ThonkPT@users.noreply.github.com>
2020-11-14 01:57:17 +0000

    a

---
+++ JS/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/JS/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
